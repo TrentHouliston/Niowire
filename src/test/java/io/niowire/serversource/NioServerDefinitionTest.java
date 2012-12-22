5dc6bdbd409233d38694095897106524db66402f
Trent Houliston <trent@houliston.me>
2012-12-22 14:46:17 +1100

    Fixed using the wrong variable in the NioServerDefinitionTest

--- src/test/java/io/niowire/serversource/NioServerDefinitionTest.java
+++ src/test/java/io/niowire/serversource/NioServerDefinitionTest.java
@@ -83,25 +83,25 @@ public class NioServerDefinitionTest
 		assertEquals("The Service Factories were not updated correctly", def2.getServiceFactories().get(0), def1.getServiceFactories().get(0));
 
 		//Setup our def3 and def4 with an incompatibe change (different ports)
-		def1.setId("def3");
-		def1.setName("def3");
-		def1.setPort(2000);
-		def1.setInspectorFactory(new FakeNioObjectFactory<NioInspector>("def3Inspector"));
-		def1.setSerializerFactory(new FakeNioObjectFactory<NioSerializer>("def3Serializer"));
-		def1.setServiceFactories(Collections.<NioObjectFactory<NioService>>singletonList(new FakeNioObjectFactory<NioService>("def3Service")));
+		def3.setId("def3");
+		def3.setName("def3");
+		def3.setPort(2000);
+		def3.setInspectorFactory(new FakeNioObjectFactory<NioInspector>("def3Inspector"));
+		def3.setSerializerFactory(new FakeNioObjectFactory<NioSerializer>("def3Serializer"));
+		def3.setServiceFactories(Collections.<NioObjectFactory<NioService>>singletonList(new FakeNioObjectFactory<NioService>("def3Service")));
 
 		//Setup our def2
-		def2.setId("def4");
-		def2.setName("def4");
-		def2.setPort(1000);
-		def2.setInspectorFactory(new FakeNioObjectFactory<NioInspector>("def4Inspector"));
-		def2.setSerializerFactory(new FakeNioObjectFactory<NioSerializer>("def4Serializer"));
-		def2.setServiceFactories(Collections.<NioObjectFactory<NioService>>singletonList(new FakeNioObjectFactory<NioService>("def4Service")));
+		def4.setId("def4");
+		def4.setName("def4");
+		def4.setPort(1000);
+		def4.setInspectorFactory(new FakeNioObjectFactory<NioInspector>("def4Inspector"));
+		def4.setSerializerFactory(new FakeNioObjectFactory<NioSerializer>("def4Serializer"));
+		def4.setServiceFactories(Collections.<NioObjectFactory<NioService>>singletonList(new FakeNioObjectFactory<NioService>("def4Service")));
 
 		//Perform the update (should throw an exception)
 		try
 		{
-			def1.update(def2);
+			def3.update(def4);
 			fail("An unchangeable property exception should have been thrown");
 		}
 		catch (NioPropertyUnchangableException ex)
@@ -109,14 +109,14 @@ public class NioServerDefinitionTest
 		}
 
 		//Our ID should not have changed
-		assertEquals("The ID was changed when it should not have", "def3", def1.getId());
+		assertEquals("The ID was changed when it should not have", "def3", def3.getId());
 
 		//Check that the rest of the updates whent through apart from the port change
-		assertEquals("The name was not updated correctly", def2.getName(), def1.getName());
-		assertNotEquals("The port updated (should not have been)", def2.getPort(), def1.getPort());
-		assertEquals("The Inspector was not updated correctly", def2.getInspectorFactory(), def1.getInspectorFactory());
-		assertEquals("The Serializer was not updated correctly", def2.getSerializerFactory(), def1.getSerializerFactory());
-		assertEquals("The Service Factories were not updated correctly", def2.getServiceFactories().get(0), def1.getServiceFactories().get(0));
+		assertEquals("The name was not updated correctly", def4.getName(), def3.getName());
+		assertNotEquals("The port updated (should not have been)", def4.getPort(), def3.getPort());
+		assertEquals("The Inspector was not updated correctly", def4.getInspectorFactory(), def3.getInspectorFactory());
+		assertEquals("The Serializer was not updated correctly", def4.getSerializerFactory(), def3.getSerializerFactory());
+		assertEquals("The Service Factories were not updated correctly", def4.getServiceFactories().get(0), def3.getServiceFactories().get(0));
 	}
 
 	/**
